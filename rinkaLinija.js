dd9a2a5fd08d5f11476f5803af536257fa181fac
in24 <54269634+in24@users.noreply.github.com>
2019-08-28 11:48:12 +0300

    bik

--- rinkaLinija.js
+++ rinkaLinija.js
@@ -1,6 +1,5 @@
+const c = Number(prompt("Ievadi!"));
+console.log(rinkis(c));
 function rinkis(r) {
-    return (r * 1).toExponential(0);
-  }
-  
-  const c = Number(prompt("Ievadi!"));
-  console.log(rinkis(c));
+  return (r * 1).toExponential(0);
+}
